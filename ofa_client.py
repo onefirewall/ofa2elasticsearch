801dbd828531e88b704f24eb18b390e4ea711147
Sotiraki Sima <sotiraki.sima@onefirewall.com>
2020-03-08 22:55:45 +0000

    update

--- ofa_client.py
+++ ofa_client.py
@@ -5,11 +5,17 @@
 ###################################################
 import requests
 import json
+import time
+import numpy
+import datetime
 
 print("###################################################")
 print("            Download OFA IPs                       ")
 print("###################################################")
 print("")
+
+list_of_ips = 'list_of_ips.txt'
+list_of_ips_file = open(list_of_ips,'w')
 
 config_json = {}
 
@@ -26,14 +32,24 @@ while True:
     url = config_json['api_url'] + "?ts=" + str(max_ts)
     response_json = requests.get(url,  headers={"Authorization":config_json['api_jwt_key']}).json()
 
-    if len(response_json['body']) == 0:
-        break
+    code = response_json['header']['exec_python']
 
-    for i in response_json['body']: 
-        #print(i['ip'])
+    for i in response_json['body']:
+
+        scoreTimeZero = i['score']
+        current_time = int(time.time()) - int(i['ts'])
+
+        exec(code)
+        score=int(score)
+        
+        event_ts = str(datetime.datetime.fromtimestamp(i['ts']).isoformat()) + ".000Z"
+        list_of_ips_file.write(i['ip'] + " " + str(int(i['score'])) + " " + str(event_ts) + " " + str(score) + "\n")
+
         if( max_ts < i['ts']):
             max_ts = i['ts']
-
-print(response_json['header'])
+    
+    break
+    if len(response_json['body']) == 0:
+        break
 
 print("###################################################")
