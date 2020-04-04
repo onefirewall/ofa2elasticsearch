68e1531d28f82fbe9be494a428747aaed7ff7e37
Sotiraki Sima <sotiraki.sima@onefirewall.com>
2020-04-04 13:18:33 +0100

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
-#time python3 ofa_client.py
-#ls -l 
-#/usr/share/logstash/bin/logstash -f logstash.conf
+#!/bin/bash
+
 pwd
-#java -version
+# java -version
 /usr/share/logstash/bin/logstash --version
 #which logstash
-python3 ofa_2_txt_env_var.py
+#python3 ofa_2_txt_env_var.py
 ls -l
 curl http://127.0.0.1:9200/
 curl http://192.168.43.56:9200/
-echo 'end'
+#/usr/share/logstash/bin/logstash -f logstash.conf
+
+#echo 'end'
+
